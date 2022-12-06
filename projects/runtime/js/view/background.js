988a9f5f0d2fd4ca197dc8f171ec14445dce511f
DiaSco24 <scott700@stcharles.k12.la.us>
2022-12-06 14:32:22 +0000

    runtime

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,7 +26,10 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
+
+        var tree;
+        var buildings = [];
+      
      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -36,17 +39,43 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'black');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
             
-            
+            for(var stars = 0; stars <=500; stars++){
+            var circle = draw.circle(1, "white", "LightGray", 3);
+            circle.x = canvasWidth * Math.random();
+             circle.y = groundY * Math.random();
+            background.addChild(circle);
+         }
+
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = 900;
+            moon.y = 20;
+            moon.scaleX = 0.5;
+            moon.scaleY = 0.5;
+            background.addChild(moon);
+
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
+            for (var i = 0; i < 5; ++i) {
+                var buildingHeight = 300 * Math.random();
+                var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
             
             // TODO 4: Part 1 - Add a tree
             
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 10;
+            tree.y = 300;
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
@@ -61,12 +90,23 @@ var background = function (window) {
             
             // TODO 4: Part 2 - Move the tree!
             
-            
+            tree.x = tree.x - 1;
+            if (tree.x < -200) {
+            tree.x = canvasWidth;
+            }
+
             // TODO 5: Part 2 - Parallax
-            
+           
+              for (var i = 0; i < buildings.length; i++) {
+                var eachElement = buildings[i];
+                eachElement.x = eachElement.x -2;
+                if (eachElement.x < -200) {
+                    eachElement.x = canvasWidth;
+                }
+                // code to do something with each element
+              }
 
         } // end of update function - DO NOT DELETE
-        
         
         
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
--- projects/runtime/js/view/hud.js
+++ projects/runtime/js/view/hud.js
@@ -34,14 +34,14 @@
         hud = new createjs.Container();
         
         integrity = new createjs.Container();
-        background = draw.rect(104, 20, '#CCC');
+        background = draw.rect(104, 20, '#FFC0CB');
         draw.rect(102, 18, '#FFF', null, null, 1, 1, background);
         integrity.addChild(background);
         
-        txtScore = draw.textfield('score : 000', "19px Arial", '#666', 'left');
+        txtScore = draw.textfield('score : 000', "19px Arial", '#FFC0CB', 'left');
         hud.addChild(txtScore);
         
-        integrityMeter = draw.rect(1, 16, '#3333CC');
+        integrityMeter = draw.rect(1, 16, '#FFC0CB');
         integrityMeter.scaleX = 100;
         integrity.addChild(integrityMeter);
         hud.addChild(integrity);
