17e255cb903f331b7ac71cd9d48a3b66010390d5
DiaSco24 <scott700@stcharles.k12.la.us>
2024-03-15 14:15:02 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -14,20 +14,21 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+  // creates variables used in further functions throughout making the game
     var walker = {
     positionX :  0,
     speedX : 0,
     positionY : 0,
     speedY : 0,
     };
-    // creates variables used in further functions throughout making the game
+   
 
   // one-time setup
 
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);  
   $(document).on('keyup', handleKeyUp);  
+  // making the keys known for their nunber variable in order to call my funcion later
   var KEY = {
     "LEFT": 37,
     "UP": 38,
@@ -46,6 +47,7 @@ function runProgram(){
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
+ // calling my functions that i made in order for them to run
   function newFrame() {
 
     repositionwalker();
@@ -59,7 +61,7 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-
+ // when the keys are pressed the walker will move
   function handleKeyDown(event) {
     if (event.which === KEY.LEFT){
       walker.speedX = -5;
@@ -75,8 +77,6 @@ function runProgram(){
       walker.speedY = 5;
     }
   }
-
-  // when the keys are pressed the walker will move
   // whent he key is up the walker will not continue moving
   function handleKeyUp(event) {
     if (event.which === KEY.LEFT){
@@ -101,7 +101,7 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+// ends the game once its copleted/ or it hits the borders
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -111,23 +111,21 @@ function runProgram(){
   }
 
   // self explanatory, it ends the game
-
+// repositions the walker every time it respawns
   function repositionwalker() {
    
     walker.positionX += walker.speedX;
     walker.positionY += walker.speedY;
 
   }
-  // this function helps reposition the walker
-
+  // this function helps redraw the walker
   function redrawwalker(){
     $("#walker").css("left", walker.positionX);
     $("#walker").css("top", walker.positionY);
   }
-
-  // this function helps redraw the walker
   
 }
+ // this function adds borders so the walker does not go past them
   function wallCollision(){
     if (walker.positionX <= 0){
       walker.positionX = 0
@@ -141,5 +139,4 @@ function runProgram(){
     if (walker.positionY >= $("#board").height()-50){
       walker.positionY = $("#board").height()-50
     }
-   // this function adds borders so the walker does not go past them
   }
